1e7298d61d2f29b7e1286120286d670fca475ed9
Chun-Lin-Huang <elaine930729@gmail.com>
2024-11-09 01:29:07 +0800

    init

--- README.md
+++ README.md
@@ -1,50 +1,28 @@
-# React + TypeScript + Vite
+# 第三組期中專案-故事書
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+## 網站的設計概念
+將閱讀轉化為一種全新的互動體驗。透過場景互動，網站不僅提供故事內容，更讓使用者成為故事的一部分。每個故事場景都充滿了動畫元素，使用者可以點擊、拖動，甚至參與小遊戲來推進情節。這種沉浸式的體驗讓閱讀更加生動有趣，同時增強了學習效果，適合兒童和家長共讀，增進感情。
 
-Currently, two official plugins are available:
+我們的設計強調簡單直觀的使用者界面，讓孩子能輕鬆理解與操作，同時為家長提供方便的工具來協助與孩子進行互動。網站採用了色彩豐富且具有吸引力的視覺設計，以提升孩子的參與度，並結合互動遊戲增強體驗感。
 
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+### 關鍵特點：
+(1)互動式故事書：藉由互動元素（如選擇式對話）讓故事更生動。
+(2)親子模式：設計專門的功能，讓家長可以與孩子一起閱讀和進行故事相關的遊戲或問題討論。
 
-## Expanding the ESLint configuration
+## 設計原則
+(1)以孩子為中心：簡單、直觀的導航和互動方式。
+(2)教育與娛樂結合：在遊戲和故事中加入教育元素，幫助孩子學習。
+(2)親子互動：設計適合雙人閱讀和合作完成的小任務，促進親子互動。
 
-If you are developing a production application, we recommend updating the configuration to enable type aware lint rules:
+## 設計規劃
+網站規劃包含六個小故事的選單頁和每個故事的詳細頁。進入網站後，首先看到六個故事的封面圖，每個封面圖下方附有簡短的標題或主題說明。使用者點選任意故事後進入該故事的詳細頁，詳細頁中包含故事的簡要說明以及互動開始按鈕。按下互動開始後，故事以動態呈現的方式逐步展開，透過簡單的點擊或拖動互動，讓使用者輕鬆地探索每個場景、推進情節，體驗生動的故事內容。
 
-- Configure the top-level `parserOptions` property like this:
+## 技術選用
+為確保平台的穩定性和易用性，我們使用了以下技術：
+(1)使用 React 和 TypeScript 構建互動頁面，優化性能。
+(2)使用 HTML 構建語義化且結構清晰的網頁，為內容提供基礎結構。
+(3)使用 CSS Modules / Styled Components 使樣式更加模組化並防止衝突。
+(4)測試與反饋：進行使用者測試，根據反饋進行迭代更新。
 
-```js
-export default tseslint.config({
-  languageOptions: {
-    // other options...
-    parserOptions: {
-      project: ['./tsconfig.node.json', './tsconfig.app.json'],
-      tsconfigRootDir: import.meta.dirname,
-    },
-  },
-})
-```
-
-- Replace `tseslint.configs.recommended` to `tseslint.configs.recommendedTypeChecked` or `tseslint.configs.strictTypeChecked`
-- Optionally add `...tseslint.configs.stylisticTypeChecked`
-- Install [eslint-plugin-react](https://github.com/jsx-eslint/eslint-plugin-react) and update the config:
-
-```js
-// eslint.config.js
-import react from 'eslint-plugin-react'
-
-export default tseslint.config({
-  // Set the react version
-  settings: { react: { version: '18.3' } },
-  plugins: {
-    // Add the react plugin
-    react,
-  },
-  rules: {
-    // other rules...
-    // Enable its recommended rules
-    ...react.configs.recommended.rules,
-    ...react.configs['jsx-runtime'].rules,
-  },
-})
-```
+## 結論
+該平台旨在創建一個有趣且教育意義深遠的互動式閱讀環境，幫助家長和孩子共享寶貴的親子時光。透過React、TypeScript等現代化技術的應用，我們確保了平臺的流暢性、易用性和可持續性。
--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,131 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import book1 from './pig.jpg';
+import book2 from './redhat.jpg';
+import book3 from './princess.jpg';
+import book4 from './drinkwater.jpg';
+import book5 from './tiger.jpg';
+import book6 from './man.jpg';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <body className="homepage is-preload">
+        <div id="page-wrapper">
+          <section id="header">
+          {/* Logo */}
+            <h1>Story Book</h1> 
+          </section>    
+          {/* Banner */}
+          <section id="banner">
+            <header>
+              <h2>Hello! This is a story world.</h2>
+              <p>We hope you can happy at here!</p>
+            </header>
+          </section>
+          {/* Main */}
+          <section id="main">
+            <div className="container">
+              <div className="row">
+                <div className="col-12">
+                {/* Story */}
+                  <section>
+                    <header className="major">
+                      <h2>My Story</h2>
+                    </header>
+                    <div className="row">
+                      <div className="col-4 col-6-medium col-12-small">
+                        <section className="box">
+                          <a href="#" className="image featured"><img src={book1} alt="" /></a>
+                          <header>
+                            <h3>三隻小豬</h3>
+                          </header>
+                          <p>三隻小豬的故事講述了三兄弟各自蓋房子的經歷。大豬和二豬為了省事，分別用稻草和木頭建造房子，而小豬則辛苦地用磚頭蓋了堅固的房子。當大野狼來襲時，稻草和木頭的房子都被輕易摧毀，只有小豬的磚房抵擋住了攻擊，最終三隻小豬在安全的磚房裡團聚，學到了堅持和努力的重要性。</p>
+                          <footer>
+                            <ul className="actions">
+                              <li><a href="#" className="button alt">Read & Play</a></li>
+                            </ul>
+                          </footer>
+                        </section>
+                      </div>
+                      <div className="col-4 col-6-medium col-12-small">
+                        <section className="box">
+                          <a href="#" className="image featured"><img src={book2} alt="" /></a>
+                          <header>
+                            <h3>小紅帽</h3>
+                          </header>
+                          <p>小紅帽的故事講述了一個小女孩帶著食物去看望生病的外婆，途中遇到了一隻狡猾的大野狼。大野狼騙走了小紅帽，趕到外婆家吞下外婆，然後假扮成外婆躺在床上等待小紅帽的到來。小紅帽發現外婆的樣子奇怪，後來被大野狼吞下。幸好獵人及時趕來，成功救出了小紅帽和外婆。</p>
+                          <footer>
+                            <ul className="actions">
+                              <li><a href="#" className="button alt">Read & Play</a></li>
+                            </ul>
+                          </footer>
+                        </section>
+                      </div>
+                      <div className="col-4 col-6-medium col-12-small">
+                        <section className="box">
+                          <a href="#" className="image featured"><img src={book3} alt="" /></a>
+                          <header>
+                            <h3>灰姑娘</h3>
+                          </header>
+                          <p>灰姑娘是被繼母和繼姊妹虐待的善良女孩。王子舉辦舞會時，仙女教母用魔法幫助她變得美麗，並給她南瓜馬車，提醒她午夜前必須離開。灰姑娘在舞會上與王子共舞，兩人墜入愛河，但她在午夜鐘聲響起時匆忙離開，留下了一隻玻璃鞋。王子遍尋全國，最終兩人結婚過上幸福快樂的生活。</p>
+                          <footer>
+                            <ul className="actions">
+                              <li><a href="#" className="button alt">Read & Play</a></li>
+                            </ul>
+                          </footer>
+                        </section>
+                      </div>
+                      <div className="col-4 col-6-medium col-12-small">
+                        <section className="box">
+                          <a href="#" className="image featured"><img src={book4} alt="" /></a>
+                          <header>
+                            <h3>烏鴉喝水</h3>
+                          </header>
+                          <p>《烏鴉喝水》的故事講述了一隻口渴的烏鴉發現一個瓶子裡有少量的水，但瓶口狹窄，它無法直接喝到。烏鴉靈機一動，開始一顆一顆地撿石子丟進瓶子裡，水位逐漸上升，最後烏鴉成功喝到了水。這個寓言告訴我們：遇到困難時，要保持冷靜，運用智慧和耐心來解決問題。</p>
+                          <footer>
+                            <ul className="actions">
+                              <li><a href="#" className="button alt">Read & Play</a></li>
+                            </ul>
+                          </footer>
+                        </section>
+                      </div>
+                      <div className="col-4 col-6-medium col-12-small">
+                        <section className="box">
+                          <a href="#" className="image featured"><img src={book5} alt="" /></a>
+                          <header>
+                            <h3>虎姑婆</h3>
+                          </header>
+                          <p>《虎姑婆》是一個流傳於華人社會的民間故事，講述了兩姐弟被父母單獨留在家中時，一隻假扮成老婦人的老虎來敲門，謊稱自己是他們的姑婆，想進屋吃掉他們。機靈的姐姐察覺到虎姑婆的異樣，在夜裡設法保護弟弟並用計逃脫。最後，兩姐弟成功逃離虎姑婆的魔掌，避免了危險。</p>
+                          <footer>
+                            <ul className="actions">
+                              <li><a href="#" className="button alt">Read & Play</a></li>
+                            </ul>
+                          </footer>
+                        </section>
+                      </div>
+                      <div className="col-4 col-6-medium col-12-small">
+                        <section className="box">
+                          <a href="#" className="image featured"><img src={book6} alt="" /></a>
+                          <header>
+                            <h3>浦島太郎</h3>
+                          </header>
+                          <p>浦島太郎的故事講述了一位漁夫浦島太郎，他在海邊救了一隻被欺負的烏龜，後來烏龜帶他到龍宮城，見到了美麗的公主乙姬。浦島太郎在那裡過得非常快樂，但還是決定回到人間。乙姬給了他一個不能打開的神秘寶盒作為告別禮物，回到地面後他打開了寶盒，結果變成了白髮蒼蒼的老人。</p>
+                          <footer>
+                            <ul className="actions">
+                              <li><a href="#" className="button alt">Read & Play</a></li>
+                            </ul>
+                          </footer>
+                        </section>
+                      </div>
+                    </div>
+                  </section>   
+                </div> 
+              </div>
+            </div>
+          </section>
+        </div>    
+      </body>
     </>
   )
 }
