8b7c11f828fdc99326f0a0071ee45167f8a5a43a
rainq2 <rain0970018607@gmail.com>
2024-11-19 01:46:31 +0800

    good

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/react-ts-mid/', // 替換為你的 GitHub 儲存庫名稱
+  base: '/react-ts-mid' // 替換為你的 GitHub 儲存庫名稱
 })
